51241843f8f4bfcd523e2d47ff51e1c44e6d4128
Jinn <chernayamolnia@yandex.ru>
2019-03-27 20:35:03 +0400

    Added admin and session controllers and forbidded 'products' page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/destroy'
   resources :users
   resources :orders
   resources :line_items
   resources :carts
 
   get 'store/index'
-  
+
   resources :products do
     get :who_bought, on: :member
   end
+
+
 
   #The priority is based upon order of creation:
 #(Приоритет основан на порядке создания:)
