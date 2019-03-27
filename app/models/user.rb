bfe34e3f4ecf35b5c5f22eec8f5764e805106e3b
Jinn <chernayamolnia@yandex.ru>
2019-03-27 21:35:26 +0400

    Added protection from removing the last known user/admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  private
+    def ensure_an_admin_remains
+      if User.count.zero?
+        raise "Last user can't be deleted"
+      end
+    end
+
 end
